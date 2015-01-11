0b1a1e6ffd7bdd2c1e18bb59914e55e98872e6be
Mike King <Michael.Thomas.Kinger@gmail.com>
2015-01-10 20:30:49 -0500

    BlackJack done. No bells, no whistles. A few minor display issues with spacing, but the game itself worked for every test I ran.

---
+++ BlackJack/src/main/java/com/swcguild/blackjack/Blackjack.java
@@ -0,0 +1,109 @@
+package com.swcguild.blackjack;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Blackjack {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+
+        int playerCard1 = 2 + r.nextInt(10);
+        int playerCard2 = 2 + r.nextInt(10);
+        int dealerCard1 = 2 + r.nextInt(10);
+        int dealerCard2 = 2 + r.nextInt(10);
+        int playerTotal;
+        int dealerTotal;
+        int hitCard;
+        String playerChoice = "";
+
+        System.out.println("Welcome to Kinger's Casino!");
+        Thread.sleep(1000);
+        System.out.println("\nLet's play some Blackjack!");
+        Thread.sleep(1000);
+        System.out.println("\nYou get a " + playerCard1 + " and a " + playerCard2 + ".");
+        Thread.sleep(1000);
+        playerTotal = (playerCard1 + playerCard2);
+        System.out.println("Your total is " + playerTotal + ".");
+        Thread.sleep(1000);
+        System.out.println("");
+        System.out.println("\nThe dealer has a " + dealerCard1 + " showing, plus a hidden card.");
+        Thread.sleep(1000);
+        dealerTotal = (dealerCard1 + dealerCard2);
+        System.out.println("His total is hidden too.");
+        Thread.sleep(1000);
+
+        if (playerTotal == 21) {
+            System.out.println("\nPlayer has BLACKJACK!");
+        }
+        while (playerTotal < 21 && !playerChoice.equalsIgnoreCase("stay") && playerTotal != 0) {
+            System.out.print("\nWould you like to \"hit\" or \"stay\"? ");
+            playerChoice = sc.nextLine();
+            if (playerChoice.equalsIgnoreCase("hit")) {
+                hitCard = 2 + r.nextInt(10);
+                playerTotal = (playerTotal + hitCard);
+                Thread.sleep(1000);
+                System.out.println("\nYou drew a " + hitCard + ".");
+                Thread.sleep(1000);
+                System.out.println("Your total is " + playerTotal + ".");
+                System.out.println("");
+            } else if (playerChoice.equalsIgnoreCase("stay")) {
+                Thread.sleep(1000);
+                System.out.println("\nYou have chosen to stay with a total of " + playerTotal + ".");
+            } else {
+                System.out.println("I did not understand.");
+            }
+            if (playerTotal > 21) {
+                System.out.println("The player has busted.");
+                playerTotal = 0;
+            }
+        }
+        System.out.println("\nOkay, now it's the dealer's turn.");
+        Thread.sleep(1000);
+        System.out.println("\nHis hidden card was a " + dealerCard2 + ".");
+        Thread.sleep(1000);
+        System.out.println("That makes his total " + dealerTotal + ".");
+
+        while (dealerTotal <= 16) {
+            Thread.sleep(1000);
+            System.out.println("\n The dealer will hit");
+            Thread.sleep(1000);
+            hitCard = 2 + r.nextInt(10);
+            System.out.println("\nThe dealer's hit card was a " + hitCard + ".");
+            Thread.sleep(1000);
+            dealerTotal = (dealerTotal + hitCard);
+            System.out.println("That makes his total " + dealerTotal + ".");
+            Thread.sleep(1000);
+        }
+        if (dealerTotal > 21) {
+            System.out.println("\nThe dealer has BUSTED!!!");
+            Thread.sleep(1000);
+            dealerTotal = 0;
+        } else {
+            System.out.println("\nThe dealer stays.");
+            Thread.sleep(1000);
+        }
+        if (playerTotal == 0) {
+            System.out.println("\nThe player busted.");
+            Thread.sleep(1000);
+        } else {
+            System.out.println("\nThe player has " + playerTotal + ".");
+            Thread.sleep(1000);
+        }
+        if (dealerTotal == 0) {
+            System.out.println("\nThe dealer busted.");
+            Thread.sleep(1000);
+        } else {
+            System.out.println("\nThe dealer has " + dealerTotal + ".");
+        }
+        if (playerTotal > dealerTotal) {
+            System.out.println("\n PLAYER WINS!!!");
+        } else {
+            System.out.println("\nThe dealer wins. Better luck next time.");
+        }
+
+    }
+
+}
