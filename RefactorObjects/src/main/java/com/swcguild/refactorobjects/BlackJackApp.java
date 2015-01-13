ee11ad7d45354a96b485f904a3b4a3d9f280a09b
Mike King <Michael.Thomas.Kinger@gmail.com>
2015-01-12 23:48:50 -0500

    Factored BlackJack as well now that I get it.

---
+++ RefactorObjects/src/main/java/com/swcguild/refactorobjects/BlackJackApp.java
@@ -0,0 +1,10 @@
+package com.swcguild.refactorobjects;
+
+public class BlackJackApp {
+    
+    public static void main(String[] args) throws Exception{
+        BlackJack bJack = new BlackJack();
+        bJack.run();
+    }
+
+}
