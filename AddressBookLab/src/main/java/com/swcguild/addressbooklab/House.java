b0e98394cd1abbaf3adf53e157e8cda2cce1b50b
Mike King <Michael.Thomas.Kinger@gmail.com>
2015-01-20 21:01:37 -0500

    Address Book - with JUnit.

--- AddressBookLab/src/main/java/com/swcguild/addressbooklab/House.java
+++ AddressBookLab/src/main/java/com/swcguild/addressbooklab/House.java
@@ -1,5 +1,7 @@
 package com.swcguild.addressbooklab;
 
+import java.util.Objects;
+
 public class House {
 
     private final String lastName;
@@ -57,4 +59,53 @@ public class House {
         this.zip = zip;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.lastName);
+        hash = 79 * hash + Objects.hashCode(this.firstName);
+        hash = 79 * hash + Objects.hashCode(this.streetAddress);
+        hash = 79 * hash + Objects.hashCode(this.city);
+        hash = 79 * hash + Objects.hashCode(this.state);
+        hash = 79 * hash + Objects.hashCode(this.zip);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final House other = (House) obj;
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.streetAddress, other.streetAddress)) {
+            return false;
+        }
+        if (!Objects.equals(this.city, other.city)) {
+            return false;
+        }
+        if (!Objects.equals(this.state, other.state)) {
+            return false;
+        }
+        if (!Objects.equals(this.zip, other.zip)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
+    
+    
+    
+    
+    
 }
--- AddressBookLab/src/test/java/com/swcguild/addressbooklab/AddressBookTest.java
+++ AddressBookLab/src/test/java/com/swcguild/addressbooklab/AddressBookTest.java
@@ -5,8 +5,10 @@
  */
 package com.swcguild.addressbooklab;
 
+import java.io.IOException;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,9 +17,9 @@ import org.junit.Test;
  *
  * @author apprentice
  */
-public class NewEmptyJUnitTest {
+public class AddressBookTest {
 
-    public NewEmptyJUnitTest() {
+    public AddressBookTest() {
     }
 
     @BeforeClass
@@ -42,7 +44,23 @@ public class NewEmptyJUnitTest {
     // @Test
     // public void hello() {}
     @Test
-    public void getNumberHousesTest() {
+    public void addressBookTest() throws IOException {
+        AddressBook ab = new AddressBook();
+        House testHouse = new House("King");
+        testHouse.setFirstName("Mike");
+        testHouse.setStreetAddress("401 South Main St.");
+        testHouse.setCity("Akron");
+        testHouse.setState("OH");
+        testHouse.setZip("44311");
 
+        ab.addHouse("King", testHouse);
+
+        ab.writeAddressBook();
+
+        AddressBook ab1 = new AddressBook();
+
+        ab1.loadAddressBook();
+
+        //assertEquals(ab.getHouse("King"), ab1.getHouse("King"));
     }
 }
