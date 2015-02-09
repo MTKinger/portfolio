a2e6e90881f0830154396f7bd8859f3b18118a75
Mike King <Michael.Thomas.Kinger@gmail.com>
2015-02-09 11:59:10 -0500

    Console IO updated to include LocalDate.

--- ConsoleIO/src/main/java/com/swcguild/consoleio/ConsoleIO.java
+++ ConsoleIO/src/main/java/com/swcguild/consoleio/ConsoleIO.java
@@ -1,12 +1,15 @@
 package com.swcguild.consoleio;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class ConsoleIO {
+public class ConsoleIO implements ConsoleIOInterface {
 
     Scanner sc = new Scanner(System.in);
 
+    @Override
     public int getInt(String prompt) {
         boolean badInput;
         int result = 0;
@@ -24,6 +27,7 @@ public class ConsoleIO {
         return result;
     }
 
+    @Override
     public int getInt(String prompt, int min, int max) {
         int input = 0;
         boolean badInput;
@@ -44,12 +48,14 @@ public class ConsoleIO {
         return input;
     }
 
+    @Override
     public String getString(String prompt) {
         System.out.println(prompt);
         String input = sc.nextLine();
         return input;
     }
 
+    @Override
     public float getFloat(String prompt) {
         boolean badInput;
         float input = 0;
@@ -59,7 +65,7 @@ public class ConsoleIO {
                 System.out.print(prompt);
                 input = Float.parseFloat(sc.nextLine());
                 badInput = false;
-            } catch (InputMismatchException ime) {
+            } catch (NumberFormatException nfe) {
                 System.out.println("Please enter a valid numerical value!");
                 badInput = true;
             }
@@ -67,6 +73,7 @@ public class ConsoleIO {
         return input;
     }
 
+    @Override
     public float getFloat(String prompt, float min, float max) {
         float input = 0;
         boolean badInput;
@@ -85,6 +92,7 @@ public class ConsoleIO {
         return input;
     }
 
+    @Override
     public double getDouble(String prompt) {
         boolean badInput;
         double input = 0;
@@ -102,6 +110,7 @@ public class ConsoleIO {
         return input;
     }
 
+    @Override
     public double getDouble(String prompt, double min, double max) {
         double input = 0;
         boolean badInput;
@@ -123,11 +132,32 @@ public class ConsoleIO {
         return input;
     }
 
+    @Override
     public void printMessage(String prompt) {
         System.out.println(prompt);
     }
 
+    @Override
     public void printMessageNoReturn(String prompt) {
         System.out.print(prompt);
+    }
+    
+    @Override
+    public LocalDate getIsoDate(String prompt) {
+        boolean badInput = false;
+        LocalDate result = null;
+        do{
+            try{
+                System.out.println(prompt);
+                String dateString = sc.nextLine();
+                result = LocalDate.parse(dateString);
+                badInput = false;
+            }
+            catch(DateTimeParseException dtpe){
+                System.out.println("This is not a valid date. Please enter another date.");
+                badInput = true;
+            }
+        }while (badInput == true);
+        return result;
     }
 }
---
+++ ConsoleIO/src/main/java/com/swcguild/consoleio/ConsoleIOInterface.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.swcguild.consoleio;
+
+import java.time.LocalDate;
+
+
+public interface ConsoleIOInterface {
+
+    double getDouble(String prompt);
+
+    double getDouble(String prompt, double min, double max);
+
+    float getFloat(String prompt);
+
+    float getFloat(String prompt, float min, float max);
+
+    int getInt(String prompt);
+
+    int getInt(String prompt, int min, int max);
+
+    String getString(String prompt);
+
+    void printMessage(String prompt);
+
+    void printMessageNoReturn(String prompt);
+    
+    LocalDate getIsoDate(String prompt);
+    
+}
